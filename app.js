c5dfc098d6d8d9a7064068d70be0d2394ca182e8
KibinCrew <mkevingalang@gmail.com>
2025-06-27 13:07:13 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var myLogModule = require('./PRELIMS/utility/log.js');
+
+// myLogModule.warning('Warning node not found..');
+// myLogModule.error('Error: node encountered and error..');
+// myLogModule.info('Node running...');
+
+var msg = require('./PRELIMS/messages.js');
+console.log(msg);
